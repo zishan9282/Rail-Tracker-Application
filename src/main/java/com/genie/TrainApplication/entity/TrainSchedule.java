22d0f6d24a212d9a62901746914e97c914ce96a6
Zishan <zishan.ahmad160452@gmail.com>
2026-01-21 13:19:37 +0530

    Update TrainSchedule.java

--- src/main/java/com/genie/TrainApplication/entity/TrainSchedule.java
+++ src/main/java/com/genie/TrainApplication/entity/TrainSchedule.java
@@ -1,5 +1,6 @@
 package com.genie.TrainApplication.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Data;
